7f51ca2d6658426e1da8c6e7b68db9894cd5c7b4
dreamer2018shine <2099557384@qq.com>
2018-03-30 18:14:49 +0800

    fourth commit

--- DREAMLIST/app/src/main/java/activitytest/example/com/dreamlist/MainActivity.java
+++ DREAMLIST/app/src/main/java/activitytest/example/com/dreamlist/MainActivity.java
@@ -1,6 +1,7 @@
 package activitytest.example.com.dreamlist;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import activitytest.example.com.dreamlist.ui.AchievementActivity;
@@ -25,21 +27,22 @@ import static activitytest.example.com.dreamlist.R.id.toolbar;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
+    //初始化导航试图和抽屉菜单
     private DrawerLayout drawerLayout;
-    private NavigationView navigation;
-    private ActionBarDrawerToggle drawerToggle;
-
+    private NavigationView navigationView;
+    //关联菜单按钮
+    ImageView menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.drawerlayout_main);
+        setContentView(R.layout.activity_main);
         //点击按钮，触发监听事件
-        Button dreamlist= (Button) findViewById(R.id.dreamlist);
-        Button todayplan= (Button) findViewById(R.id.todayplan);
-        Button checkout= (Button) findViewById(R.id.checkout);
-        Button memory= (Button) findViewById(R.id.memory);
-        Button achievement= (Button) findViewById(R.id.achievement);
-        Button encouragement= (Button) findViewById(R.id.encouragement);
+        Button dreamlist = (Button) findViewById(R.id.dreamlist);
+        Button todayplan = (Button) findViewById(R.id.todayplan);
+        Button checkout = (Button) findViewById(R.id.checkout);
+        Button memory = (Button) findViewById(R.id.memory);
+        Button achievement = (Button) findViewById(R.id.achievement);
+        Button encouragement = (Button) findViewById(R.id.encouragement);
         dreamlist.setOnClickListener(this);
         todayplan.setOnClickListener(this);
         checkout.setOnClickListener(this);
@@ -47,72 +50,131 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         achievement.setOnClickListener(this);
         encouragement.setOnClickListener(this);
 
-        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //设置主标题颜色
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
         //设置导航菜单
-        toolbar.setNavigationIcon(R.drawable.ic_menu);
-//        对NavigationIcon添加点击
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+//        toolbar.setNavigationIcon(R.drawable.ic_menu);
 
+
+        //ID与视图关联
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.drawer_navigation);
+        //与按的菜单按钮关联
+        menu = (ImageView) findViewById(R.id.main_menu);
+
+
+        //获取头布局
+        View headerView = navigationView.getHeaderView(0);
+        menu.setOnClickListener(this);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                //item.setChecked(true);
+                //写item的点击事件触发了什么
+                Toast.makeText(MainActivity.this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                //关闭抽屉菜单
+                drawerLayout.closeDrawer(navigationView);
+                return true;
             }
         });
+        //获得抽屉布局
+//                drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        //在布局文件中生命DrawerLayout后，即可从边缘滑出抽屉了
+//
+//        //ActionBarDrawerToggle作用是在toolbar上创建一个点击弹出drawer的按钮而已
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawerLayout.addDrawerListener(toggle);
+//        //不写这句话，是没有按钮显示的
+//        toggle.syncState();
+
+
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.drawer_navigation);
+//        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+
+
+////        对NavigationIcon添加点击
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switch (v.getId()){
+//                    case R.id.main_menu:
+//                        //点击菜单，跳出侧滑菜单
+//                        // DrawerLayout.closeDrawer方法用于隐藏侧边菜单，
+//                        // DrawerLayout.openDrawer方法用于展开侧边菜单
+//                        if (drawerLayout.isDrawerOpen(navigationView)){
+//                            drawerLayout.closeDrawer(navigationView);
+//                        }else{
+//                            drawerLayout.openDrawer(navigationView);
+//                        }
+//                        break;
+//                }
+//            }
+//        });
         //初始化ActionBarDrawerToggle(ActionBarDrawerToggle就是一个开关一样用来打开或者关闭drawer)
-        drawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,toolbar,R.string.openString,R.string.closeString){
+//        drawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,toolbar,R.string.openString,R.string.closeString){
             /*
             * 抽屉菜单打开监听
             * */
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                Toast.makeText(MainActivity.this,"菜单打开了",Toast.LENGTH_SHORT).show();
-                super.onDrawerOpened(drawerView);
-            }
-            /*
-            * 抽屉菜单关闭监听
-            * */
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                Toast.makeText(MainActivity.this,"菜单关闭了",Toast.LENGTH_SHORT).show();
-                super.onDrawerClosed(drawerView);
-            }
-        };
+//            @Override
+//            public void onDrawerOpened(View drawerView) {
+//                Toast.makeText(MainActivity.this,"菜单打开了",Toast.LENGTH_SHORT).show();
+//                super.onDrawerOpened(drawerView);
+//            }
+//            /*
+//            * 抽屉菜单关闭监听
+//            * */
+//            @Override
+//            public void onDrawerClosed(View drawerView) {
+//                Toast.makeText(MainActivity.this,"菜单关闭了",Toast.LENGTH_SHORT).show();
+//                super.onDrawerClosed(drawerView);
+//            }
+//        };
         /*
         * NavigationView设置点击监听
         * */
 
         // navigation这个对象为null 因为你没有写fvb()!
-        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                Toast.makeText(MainActivity.this,menuItem.getTitle(), Toast.LENGTH_SHORT).show();
-                menuItem.setChecked(true);
-                drawerLayout.closeDrawers();
-                return false;
-            }
-        });
-        drawerToggle.syncState();
+//        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//
+//        public boolean onNavigationItemSelected(MenuItem menuItem) {
+//                Toast.makeText(MainActivity.this,menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+//                menuItem.setChecked(true);
+//                drawerLayout.closeDrawers();
+//                return false;
+//            }
+//        };
+//        drawerToggle.syncState();
         //设置DrawerLayout的抽屉开关监听
-        drawerLayout.setDrawerListener(drawerToggle);
+//        drawerLayout.setDrawerListener(drawerToggle);
 
 
-    }
+
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//
+//        }
+//    }
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+//    public boolean onCreateOptionsMenu(Menu menu) {
+////        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
 
     /*
         * 初始化布局控件
         * */
-    private void initView() {
-//        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigation = (NavigationView) findViewById(R.id.drawer_navigation);
-    }
+//    private void initView() {
+////        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        navigationView = (NavigationView) findViewById(R.id.drawer_navigation);
+//    }
     public void onClick(View v)
     {
         switch (v.getId())
@@ -140,6 +202,16 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             case R.id.encouragement:
                 Intent intent6 = new Intent(MainActivity.this, EncouragementActivity.class);
                 startActivity(intent6);
+                break;
+            case R.id.main_menu:
+                //点击菜单，跳出侧滑菜单
+                // DrawerLayout.closeDrawer方法用于隐藏侧边菜单，
+                // DrawerLayout.openDrawer方法用于展开侧边菜单
+                if (drawerLayout.isDrawerOpen(navigationView)){
+                    drawerLayout.closeDrawer(navigationView);
+                }else{
+                    drawerLayout.openDrawer(navigationView);
+                }
                 break;
         }
 
--- DREAMLIST/app/src/main/java/activitytest/example/com/dreamlist/ui/BaseActivity.java
+++ DREAMLIST/app/src/main/java/activitytest/example/com/dreamlist/ui/BaseActivity.java
@@ -1,14 +1,17 @@
 package activitytest.example.com.dreamlist.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import activitytest.example.com.dreamlist.MainActivity;
 import activitytest.example.com.dreamlist.R;
