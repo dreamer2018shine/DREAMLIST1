883e4447d661201fd5815e93a17d3ddd4c8d6f74
dreamer2018shine <2099557384@qq.com>
2018-03-31 19:28:29 +0800

    fifth commit

--- DREAMLIST/app/src/main/java/activitytest/example/com/dreamlist/entity/MyUser.java
+++ DREAMLIST/app/src/main/java/activitytest/example/com/dreamlist/entity/MyUser.java
@@ -9,4 +9,48 @@ import cn.bmob.v3.BmobUser;
 public class MyUser extends BmobUser {
 
 
+//    private String username;
+    private int age;
+    private boolean sex;
+    private String desc;
+//    public String getusername() {
+//        return username;
+//    }
+//
+//    public void setusername(String username) {
+//        this.username = username;
+//    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+//    public String getSex() {
+//        return sex;
+//    }
+//
+//    public void setSex(String sex) {
+//        this.sex = sex;
+//    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
 }
